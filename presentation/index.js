be8c73989d0fcfae2c77b6a4873e02ce18d2233e
Tirell M <tdmckinn@gmail.com>
2016-03-22 21:19:41 -0400

    Add more bg-images and code snippets

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
-// Import React
 import React from "react";
+import Iframe from 'react-iframe';
 
 // Import Spectacle Core tags
 import { Appear, BlockQuote, Cite, CodePane, Deck,
@@ -8,14 +8,9 @@ import { Appear, BlockQuote, Cite, CodePane, Deck,
          Spectacle, Text
 } from "spectacle";
 
-// Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
-
-// Import theme
 import createTheme from "spectacle/lib/themes/default";
-
-// Import custom component
-import Interactive from "../assets/interactive";
+import Interactive from "../src/interactive";
 
 // Require CSS
 require('normalize.css');
@@ -28,6 +23,11 @@ const images = {
   markdown: require('../assets/markdown.png'),
   reactLogo: require('../assets/reactjs-logo.svg'),
   reactEco: require('../assets/react-eco.png'),
+  reactPic: require('../assets/reactpic.jpeg'),
+  facebook: require('../assets/facebook.png'),
+  airbnb: require('../assets/airbnb.png'),
+  dropbox: require('../assets/dropbox.png'),
+  flowType: require('../assets/flow-hero-logo.png')
 };
 
 const gifs = {
@@ -66,7 +66,7 @@ export default class Presentation extends React.Component {
           </Slide>
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Heading size={1} caps textColor="primary" textFont="primary">
-              Plan
+              Agenda
             </Heading>
             <div style={{color: '#fff'}}>
               <ol>
@@ -80,43 +80,44 @@ export default class Presentation extends React.Component {
           </Slide>
           <Slide transition={["zoom"]} bgColor="black" bgImage={gifs.idea}>
           </Slide>
-          <Slide transition={["zoom"]} bgColor="black" bgImage={gifs.rockets}>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="black" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          {/*<Slide transition={["zoom"]} bgColor="black" bgImage={gifs.rockets}>
+          </Slide>*/}
+          <Slide transition={["zoom", "fade"]} bgImage={images.reactPic.replace("/", "")} bgDarken={0.75} notes="<ul><li>talk about that</li><li>and that</li></ul>">
             <Heading size={1} caps textColor="primary" textFont="primary">
               ReactJS
             </Heading>
           </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
+          <Slide transition={["slide"]} bgColor="black">
+           <Layout>
+            <Fill>
+              <CodePane
+                lang="jsx"
+                source={require("raw!../assets/html-jsx.example")}
+                margin="20px 1px"
+              />
+            <Text><Link textColor="primary" href="https://babeljs.io/repl/"> JSX to JS Transformer</Link></Text>
+            </Fill>
+            <Fill>
+              <CodePane
+                lang="jsx"
+                source={require("raw!../assets/jsx-js.example")}
+                margin="20px 9px"
+              />
+           </Fill>
+          </Layout> 
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps >GraphQL</Heading>
+          <Slide transition={["zoom", "fade"]} bgImage={images.city.replace("/", "")} bgDarken={0.85}>
+            <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+              What is GraphQL?
+            </Heading>
             <Layout>
               <Fill>
-                  <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                    What is GraphQL?
-                  </Heading>
-                  <Text>GraphQL is a query language created by Facebook in 2012 which provides a common interface between the client and the server for data fetching and manipulations.
+                  <Text textColor="white">GraphQL is a query language created by Facebook in 2012 which provides a common interface between the client and the server for data fetching and manipulations.
                   </Text>
               </Fill>
               <Fill>
-                <Text>Key concepts of the GraphQL query language are:</Text>
-                <List>
+                <Text textColor="white">Key concepts of the GraphQL query language are:</Text>
+                <List textColor="white">
                   <ListItem>Hierarchical</ListItem>
                   <ListItem>Product‐centric</ListItem>
                   <ListItem>Strong‐typing</ListItem>
@@ -126,44 +127,13 @@ export default class Presentation extends React.Component {
               </Fill>
             </Layout>
           </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
+          <Slide transition={["slide"]} bgColor="black">
+           <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+              What is Relay?
             </Heading>
           </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
+          <Slide transition={["slide"]} bgColor="primary" bgImage={images.flowType.replace("/", "")} bgDarken={0.85}>
+            
           </Slide>
           <Slide transition={["zoom"]} bgColor="black" >
             <Heading size={1} caps fit textColor="tertiary">
@@ -172,15 +142,52 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
           </Slide>
           <Slide transition={["zoom"]} bgColor="black" bgImage={gifs.easter}>
           </Slide>
+          <Slide transition={["zoom"]} bgColor="black">
+            <Heading size={1} caps fit textColor="tertiary">
+              Honoroable Mentions...
+            </Heading>
+             <List>
+                <ListItem>React Native</ListItem>
+                <ListItem>Redux</ListItem>
+                <ListItem>Typescript</ListItem>
+                <ListItem>Faclorjs By Netflix</ListItem>
+              </List>
+          </Slide>
           <Slide transition={["spin", "slide"]} bgColor="primary">
-            <Heading size={2} caps fit lineHeight={1} textColor="white">
+            <Heading size={1} caps lineHeight={1} textColor="white">
               Thank You
             </Heading>
-            <List>
-              <ListItem><Link href="https://github.com/tdmckinn/jurastic-waves">App Demo Jurrasic Waves</Link></ListItem>
-              <ListItem><Link href="http://www.formidablelabs.com">Github: @tdmckinn</Link></ListItem>
-              <ListItem><Link href="http://www.formidablelabs.com">Twitter: @TirellMckinnon</Link></ListItem>
-            </List>
+            <Layout>
+              <Fill>
+              <Text> Me: </Text>
+                <List textSize="12px">
+                  <ListItem><Link href="https://github.com/tdmckinn/jurastic-waves">Jurrasic Waves</Link></ListItem>
+                  <ListItem><Link href="http://www.formidablelabs.com">Github: @tdmckinn</Link></ListItem>
+                  <ListItem><Link href="http://www.formidablelabs.com">Twitter: @TirellMckinnon</Link></ListItem>
+                </List>
+              </Fill>
+              <Fill>
+                <Text>Resources =></Text>
+                <List textSize="12px">
+                  <ListItem><Link href="https://facebook.github.io/react/">Reactjs</Link></ListItem>
+                  <ListItem><Link href="https://facebook.github.io/relay/">Relay</Link></ListItem>
+                  <ListItem><Link href="http://graphql.org/">GraphQL</Link></ListItem>
+                  <ListItem><Link href="http://flowtype.org/">Flow</Link></ListItem>
+                  <ListItem><Link href="https://github.com/FormidableLabs/spectacle">Spectacle Presentation Library</Link></ListItem>
+                  <ListItem><Link href="https://stackoverflow.com/research/developer-survey-2016">Stackoverflow Devloper Survey</Link></ListItem>
+                </List>
+              </Fill>
+               <Fill>
+                <Text>Resources Cont..</Text>
+                <List textSize="12px">
+                  <ListItem><Link href="https://github.com/reactjs">React Community aka (rackt)</Link></ListItem>
+                  <ListItem><Link href="https://github.com/enaqx/awesome-react">Awseome React</Link></ListItem>
+                  <ListItem><Link href="https://github.com/chentsulin/awesome-graphql">Awseome GraphQL</Link></ListItem>
+                  <ListItem><Link href="https://github.com/FormidableLabs/spectacle">Spectacle Presentation Library</Link></ListItem>
+                  <ListItem><Link href="https://stackoverflow.com/research/developer-survey-2016">Stackoverflow Devloper Survey</Link></ListItem>
+                </List>
+              </Fill>
+            </Layout> 
           </Slide>
         </Deck>
       </Spectacle>
