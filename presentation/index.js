dfa8ab441f01ea06c6c9854dd780547baa3004ed
Tirell M <tdmckinn@gmail.com>
2016-03-24 09:02:21 -0400

    GraphQL and Relay examples

--- presentation/index.js
+++ presentation/index.js
@@ -26,7 +26,8 @@ const images = {
   facebook: require('../assets/facebook.png'),
   airbnb: require('../assets/airbnb.png'),
   dropbox: require('../assets/dropbox.png'),
-  flowType: require('../assets/flow-hero-logo.png')
+  flowType: require('../assets/flow-hero-logo.png'),
+  jurrasicWave: require('../assets/jurrasic-wave-v1.png')
 };
 
 const gifs = {
@@ -107,23 +108,23 @@ export default class Presentation extends React.Component {
           </Layout> 
           </Slide>
           <Slide transition={["slide"]} bgColor="black">
-          <Text textColor="white">Container / Functional / Pure / Impure </Text>
-           <Layout>
-            <Fill>
-              <CodePane
-                lang="jsx"
-                source={require("raw!../assets/code/stateful.example")}
-                margin="20px 1px"
-              />
+            <Text textColor="white">Container / Functional / Pure / Impure </Text>
+            <Layout>
+              <Fill>
+                <CodePane
+                  lang="jsx"
+                  source={require("raw!../assets/code/stateful.example")}
+                  margin="20px 1px"
+                />
+              </Fill>
+              <Fill>
+                <CodePane
+                  lang="jsx"
+                  source={require("raw!../assets/code/stateless.example")}
+                  margin="20px 9px"
+                />
             </Fill>
-            <Fill>
-              <CodePane
-                lang="jsx"
-                source={require("raw!../assets/code/stateless.example")}
-                margin="20px 9px"
-              />
-           </Fill>
-          </Layout> 
+            </Layout> 
           </Slide>
           <Slide transition={["zoom"]} bgColor="black">
            <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
@@ -156,17 +157,69 @@ export default class Presentation extends React.Component {
             </Layout>
           </Slide>
           <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps textColor="primary" textFont="primary">
+              GraphQL Getting Started...
+            </Heading>
+            <List textColor="white">
+              <ListItem>A GraphQL Schema: A description of your data model with an associated set of resolve methods that know how to fetch any data your application could ever need.</ListItem>
+              <ListItem>A GraphQL Server: Any server can be taught to load a schema and speak GraphQL</ListItem>
+              <ListItem>Relay: Relay speaks to GraphQL servers through a network layer. The network layer that ships with Relay is compatible with express-graphql out of the box, and will continue to evolve as we add new features to the transport.</ListItem>
+            </List>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Text textColor="white">GrahpQL Example </Text>
+            <Layout>
+              <Fill>
+                <CodePane
+                  lang="jsx"
+                  source={require("raw!../assets/code/graphQL.example")}
+                  margin="20px 1px"
+                />
+              </Fill>
+            </Layout> 
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
            <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
               What is Relay?
             </Heading>
+            <Layout>
+              <Fill>
+                  <Text textColor="white">GraphQL is a query language created by Facebook in 2012 which provides a common interface between the client and the server for data fetching and manipulations.
+                  </Text>
+              </Fill>
+              <Fill>
+                <Text textColor="white">Key concepts of the GraphQL query language are:</Text>
+                <List textColor="white">
+                  <ListItem>Hierarchical</ListItem>
+                  <ListItem>Product‐centric</ListItem>
+                  <ListItem>Strong‐typing</ListItem>
+                  <ListItem>Client‐specified queries</ListItem>
+                  <ListItem>Introspective</ListItem>
+                </List>
+              </Fill>
+            </Layout>
           </Slide>
-          <Slide transition={["slide"]} bgColor="primary" bgImage={images.flowType.replace("/", "")} bgDarken={0.85}>
-            
+           <Slide transition={["slide"]} bgColor="black">
+            <Text textColor="white">Relay Example </Text>
+            <Layout>
+              <Fill>
+                <CodePane
+                  lang="jsx"
+                  source={require("raw!../assets/code/relay.example")}
+                  margin="20px 1px"
+                />
+              </Fill>
+            </Layout> 
           </Slide>
+          {/*<Slide transition={["slide"]} bgColor="primary" bgImage={images.flowType.replace("/", "")} bgDarken={0.85}>
+  
+          </Slide>*/}
+          
           <Slide transition={["zoom"]} bgColor="black" >
-            <Heading size={1} caps fit textColor="tertiary">
+            <Heading size={1} textColor="tertiary">
               App Demo...
             </Heading>
+            <img src={images.jurrasicWave.replace("/", "") } style={{maxWidth: 800}} />
           </Slide>
           <Slide transition={["zoom"]} bgColor="black" bgImage={gifs.easter}>
           </Slide>
