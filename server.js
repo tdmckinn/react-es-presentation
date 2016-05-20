275d52779bc804eeb410ce70d8526e27a8c33c19
Tirell M <tdmckinn@gmail.com>
2016-05-19 22:27:35 -0600

    Change index path

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(require("webpack-dev-middleware")(compiler, {
 app.use(require("webpack-hot-middleware")(compiler));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(serverPort, "localhost", function (err) {
